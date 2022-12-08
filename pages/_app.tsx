78442c6b94ea621a3cfd4b039e818e6d8acf46e1
Ken Rimple <krimple@chariotsolutions.com>
2022-12-08 10:11:23 -0500

    trying amplify env pull

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps}: any) {
   useEffect(() => {
     function doSomething() {
       Amplify.configure({
-        ...awsExports, ssr: false
+        ...awsExports, ssr: true
       });
       setLoaded(true);
     }
